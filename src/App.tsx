e1cb67fc9b3c368f178d3f1f9692a877a51a9fe1
MaitriRajpara <maitri.rajpara@simformsolutions.com>
2025-05-28 13:32:30 +0530

    Add Vite Project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import myIcon from './assets/star.svg';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello React with vite</h1>
+      <img src={myIcon} alt="My Icon" width={100} height={100} />
+    </div>
+  );
+}
+
+export default App
